ea4eb9da8ca639d3a50f233068f97e2cb559353a
m1400b <m1400b@gmail.com>
2026-09-01 23:13:38 +0330

    Set default history consent prompt

--- core/settings/settings_defaults.py
+++ core/settings/settings_defaults.py
@@ -19,186 +19,62 @@ class SettingsDefaults:
     """
 
     DEFAULTS = {
+        SettingsKeys.LANGUAGE: "fa-IR",
+        SettingsKeys.THEME: "System",
+        SettingsKeys.STARTUP_MODE: "home",
+        SettingsKeys.HOME_PAGE: "https://www.google.com",
+        SettingsKeys.RESTORE_SESSION: True,
+        SettingsKeys.CHECK_UPDATES: True,
 
-        # -------------------------------------------------
-        # General
-        # -------------------------------------------------
+        SettingsKeys.ZOOM_FACTOR: 1.0,
+        SettingsKeys.UI_SCALE: 1.0,
+        SettingsKeys.SHOW_STATUS_BAR: True,
+        SettingsKeys.SHOW_BOOKMARK_BAR: True,
 
-        SettingsKeys.LANGUAGE:
-            "fa-IR",
+        SettingsKeys.DOWNLOAD_PATH: str(Path.home() / "Downloads"),
+        SettingsKeys.ASK_DOWNLOAD_LOCATION: True,
+        SettingsKeys.OPEN_AFTER_DOWNLOAD: False,
 
-        SettingsKeys.THEME:
-            "System",
+        SettingsKeys.ACCEPT_COOKIES: True,
+        SettingsKeys.SAVE_HISTORY: True,
+        SettingsKeys.ASK_SAVE_HISTORY: True,
+        SettingsKeys.SAVE_PASSWORDS: False,
+        SettingsKeys.SEND_DO_NOT_TRACK: False,
+        SettingsKeys.CLEAR_CACHE_ON_EXIT: False,
+        SettingsKeys.CLEAR_COOKIES_ON_EXIT: False,
 
-        SettingsKeys.STARTUP_MODE:
-            "home",
+        SettingsKeys.SEARCH_ENGINE: "google",
+        SettingsKeys.SEARCH_URL: "",
 
-        SettingsKeys.HOME_PAGE:
-            "https://www.google.com",
+        SettingsKeys.OPEN_LINKS_IN_BACKGROUND: False,
+        SettingsKeys.CONFIRM_CLOSE_MULTIPLE: True,
+        SettingsKeys.TAB_POSITION: "top",
 
-        SettingsKeys.RESTORE_SESSION:
-            True,
+        SettingsKeys.PROXY_ENABLED: False,
+        SettingsKeys.PROXY_HOST: "",
+        SettingsKeys.PROXY_PORT: 0,
+        SettingsKeys.VPN_ENABLED: False,
 
-        SettingsKeys.CHECK_UPDATES:
-            True,
+        SettingsKeys.AI_PROVIDER: "ollama",
+        SettingsKeys.AI_MODEL: "",
+        SettingsKeys.AI_ENDPOINT: "http://127.0.0.1:11434",
+        SettingsKeys.AI_API_KEY: "",
 
-        # -------------------------------------------------
-        # Appearance
-        # -------------------------------------------------
+        SettingsKeys.ENABLE_JAVASCRIPT: True,
+        SettingsKeys.ENABLE_POPUPS: False,
+        SettingsKeys.ENABLE_PDF_VIEWER: True,
+        SettingsKeys.ENABLE_WEBRTC: True,
 
-        SettingsKeys.ZOOM_FACTOR:
-            1.0,
-
-        SettingsKeys.UI_SCALE:
-            1.0,
-
-        SettingsKeys.SHOW_STATUS_BAR:
-            True,
-
-        SettingsKeys.SHOW_BOOKMARK_BAR:
-            True,
-
-        # -------------------------------------------------
-        # Downloads
-        # -------------------------------------------------
-
-        SettingsKeys.DOWNLOAD_PATH:
-            str(Path.home() / "Downloads"),
-
-        SettingsKeys.ASK_DOWNLOAD_LOCATION:
-            True,
-
-        SettingsKeys.OPEN_AFTER_DOWNLOAD:
-            False,
-
-        # -------------------------------------------------
-        # Privacy
-        # -------------------------------------------------
-
-        SettingsKeys.ACCEPT_COOKIES:
-            True,
-
-        SettingsKeys.SAVE_HISTORY:
-            True,
-
-        SettingsKeys.SAVE_PASSWORDS:
-            False,
-
-        SettingsKeys.SEND_DO_NOT_TRACK:
-            False,
-
-        SettingsKeys.CLEAR_CACHE_ON_EXIT:
-            False,
-
-        SettingsKeys.CLEAR_COOKIES_ON_EXIT:
-            False,
-
-        # -------------------------------------------------
-        # Search
-        # -------------------------------------------------
-
-        SettingsKeys.SEARCH_ENGINE:
-            "google",
-
-        SettingsKeys.SEARCH_URL:
-            "",
-
-        # -------------------------------------------------
-        # Tabs
-        # -------------------------------------------------
-
-        SettingsKeys.OPEN_LINKS_IN_BACKGROUND:
-            False,
-
-        SettingsKeys.CONFIRM_CLOSE_MULTIPLE:
-            True,
-
-        SettingsKeys.TAB_POSITION:
-            "top",
-
-        # -------------------------------------------------
-        # Network
-        # -------------------------------------------------
-
-        SettingsKeys.PROXY_ENABLED:
-            False,
-
-        SettingsKeys.PROXY_HOST:
-            "",
-
-        SettingsKeys.PROXY_PORT:
-            0,
-
-        SettingsKeys.VPN_ENABLED:
-            False,
-
-        # -------------------------------------------------
-        # AI
-        # -------------------------------------------------
-
-        SettingsKeys.AI_PROVIDER:
-            "ollama",
-
-        SettingsKeys.AI_MODEL:
-            "",
-
-        SettingsKeys.AI_ENDPOINT:
-            "http://127.0.0.1:11434",
-
-        SettingsKeys.AI_API_KEY:
-            "",
-
-        # -------------------------------------------------
-        # Security
-        # -------------------------------------------------
-
-        SettingsKeys.ENABLE_JAVASCRIPT:
-            True,
-
-        SettingsKeys.ENABLE_POPUPS:
-            False,
-
-        SettingsKeys.ENABLE_PDF_VIEWER:
-            True,
-
-        SettingsKeys.ENABLE_WEBRTC:
-            True,
-
-        # -------------------------------------------------
-        # Advanced
-        # -------------------------------------------------
-
-        SettingsKeys.USER_AGENT:
-            "",
-
-        SettingsKeys.CACHE_SIZE:
-            512,
-
-        SettingsKeys.DEVELOPER_MODE:
-            False,
-
-        SettingsKeys.LOG_LEVEL:
-            "INFO",
+        SettingsKeys.USER_AGENT: "",
+        SettingsKeys.CACHE_SIZE: 512,
+        SettingsKeys.DEVELOPER_MODE: False,
+        SettingsKeys.LOG_LEVEL: "INFO",
     }
-
-    # -------------------------------------------------
 
     @classmethod
     def value(cls, key: str):
-
-        """
-        Return default value for a key.
-        """
-
         return cls.DEFAULTS.get(key)
-
-    # -------------------------------------------------
 
     @classmethod
     def all(cls) -> dict:
-
-        """
-        Return a copy of all default settings.
-        """
-
         return dict(cls.DEFAULTS)
